6ca61584f858279b04c7ebe746c1f1c2dafbbf60
Florentina Ilie <ilief@oregonstate.edu>
2019-10-23 08:33:50 -0700

    Fixed integer symbol class name

--- app/src/main/java/edu/osucascades/cs492/fixme07/CrimeListFragment.java
+++ app/src/main/java/edu/osucascades/cs492/fixme07/CrimeListFragment.java
@@ -78,7 +78,7 @@ public class CrimeListFragment extends Fragment {
         }
 
         @Override
-        public void onBindViewHolder(CrimeHolder holder, integer position) {
+        public void onBindViewHolder(CrimeHolder holder, int position) {
 
             Crime crime = mCrimes.get(position);
             holder.bind(crime);
